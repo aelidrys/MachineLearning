32b68beca01861d987550358b0a475ac85035270
ayoub <ayoubedark@gmail.com>
2025-07-17 13:31:23 +0100

    add focal loss in logisticReg

--- Models/LogisticRegression/focal_loss.py
+++ Models/LogisticRegression/focal_loss.py
@@ -2,6 +2,14 @@ import numpy as np
 import pandas as pd
 
 def focal_loss(y_true, y_predict, alpha=0.25, gamma=2):
-    # y_true*log(y_predict) - (1-y_true)*log(1-y_predict) Cross Entropy
-    # y_true*(1-y_predict)^gamma*log(y_predict) - (1-y_predict)*y_predict^gamma*log(1-y_predict) Focal Loss
-    pass
+
+    # Focal Loss without alpha
+    fLoss = -(
+        y_true * np.power(1-y_predict, gamma) * np.log(y_predict) + 
+        (1-y_true) * np.power(y_predict, gamma) * np.log(1-y_predict)
+    )
+    alpha_fLoss = -(
+        alpha * y_true @ np.power(1-y_predict, gamma) * np.log(y_predict) + 
+        (1-alpha) * (1-y_true) * np.power(y_predict, gamma) * np.log(1-y_predict)
+    )
+    return fLoss
